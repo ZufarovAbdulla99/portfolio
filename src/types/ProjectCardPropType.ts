a857c64915b05a8f62393c0a02142e2c8674ddda
ZufarovAbdulla <zufarovabdulla99@gmail.com>
2025-02-24 23:45:45 +0500

    work section added

---
+++ src/types/ProjectCardPropType.ts
@@ -0,0 +1,7 @@
+export interface IProjectCardPropType {
+    imgSrc: string;
+    title: string;
+    tags: string[],
+    projectLink: string;
+    classes?: string;
+}
---
+++ src/types/WorkType.ts
@@ -0,0 +1,6 @@
+export interface IWork {
+  imgSrc: string;
+  title: string;
+  tags: string[];
+  projectLink: string;
+}
