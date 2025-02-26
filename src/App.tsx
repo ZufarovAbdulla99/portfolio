e056e69d804dd31d3cae348862d10d2da520596e
ZufarovAbdulla <zufarovabdulla99@gmail.com>
2025-02-26 14:49:15 +0500

    scroll effect added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 // Node modules
+import { ReactLenis } from "lenis/react";
+import {gsap} from "gsap"
+import {ScrollTrigger} from "gsap/ScrollTrigger"
+import {useGSAP} from "@gsap/react"
 
+// Register gsap plugins
 
+gsap.registerPlugin(useGSAP, ScrollTrigger)
 
 // Components
 import Header from "./components/Header";
@@ -12,9 +18,30 @@ import Review from "./components/Review";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
+
 const App = () => {
+
+  useGSAP(() => {
+    const elements = gsap.utils.toArray('.reveal-up') as HTMLElement[];
+    
+    elements.forEach((element) => {
+      gsap.to(element, {
+        scrollTrigger: {
+          trigger: element,
+          start: '-200 bottom',
+          end: 'bottom 80%',
+          scrub: true,
+        },
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: 'power2.out'
+      })
+    })
+  })
+
   return (
-    <>
+    <ReactLenis root>
       <Header />
       <main>
         <Hero />
@@ -25,7 +52,7 @@ const App = () => {
         <Contact />
       </main>
       <Footer />
-    </>
+    </ReactLenis>
   );
 };
 
