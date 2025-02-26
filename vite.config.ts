4f3fac5de821d62f6b4d742a11f1faa6936542f0
ZufarovAbdulla <zufarovabdulla99@gmail.com>
2025-02-26 18:27:31 +0500

    configure for deploying

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['fullstackdev.uz'],
+    https: false
+  }
 })
